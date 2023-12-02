5dc4437237fc65bc06859f796195767672162fb1
ProtoDevStuff <prototypedeveloper2009@gmail.com>
2023-12-02 08:32:55 +0530

    Test #3
    base AI is completed and I'm happy with my project

--- Assets/Scripts/AnimalBehaviour.cs
+++ Assets/Scripts/AnimalBehaviour.cs
@@ -6,12 +6,11 @@ public class EnemyController : MonoBehaviour
 {
     public Transform player;
     public Rigidbody animalRb;
-    public float moveSpeed = 10f;
     public float minimumDistance = 5f;
 
     float rotationalForce = 2f;
     float latestChangeDirectionTime;
-    readonly float directionChangeTime = 3f;
+    float directionChangeTime = 3f;
     float characterVelocity = 2f;
     Vector3 movementDirection;
     Vector3 movementPerSecond;
@@ -22,13 +21,12 @@ public class EnemyController : MonoBehaviour
     }
 
     void calculateDirection(){
-        //create a random direction vector with the magnitude of 1, later multiply it with the velocity of the enemy
+        //create a random direction vector with the magnitude of 1
         movementDirection = new Vector3(Random.Range(-1.0f, 1.0f), 0f, Random.Range(-1.0f, 1.0f)).normalized;
-	    movementPerSecond = movementDirection * characterVelocity;
     }
 
     void Update(){
-        Vector3 lookAwayFromPlayerDirection = transform.position - player.position;
+        movementPerSecond = movementDirection * characterVelocity;
 
         //if the changeTime was reached, calculate a new movement vector
 	    if (Time.time - latestChangeDirectionTime > directionChangeTime){
@@ -37,22 +35,14 @@ public class EnemyController : MonoBehaviour
 	    }
 
         //check if player is nearby:
-        if(Vector3.Distance(transform.position, player.position) <= minimumDistance){
-            //rotate enemy in direction opposite to player:
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookAwayFromPlayerDirection), rotationalForce * Time.deltaTime);
+        if(Vector3.Distance(transform.position, player.position) <= minimumDistance) {characterVelocity = 10f; directionChangeTime = 0.5f; Debug.Log("Near Player");}
+        else {characterVelocity = 2f; directionChangeTime = 3f;Debug.Log("Far Player");}
 
-            //move player in direction opposite to player:
-            transform.Translate(moveSpeed * lookAwayFromPlayerDirection * Time.deltaTime);
-            Debug.DrawLine(transform.position, player.position, Color.blue, 2f);
-            Debug.Log("Rigidbody Move Position" + Time.time);
-        }
-        else{
-            //move enemy: 
-	        transform.position = new Vector3(transform.position.x + (movementPerSecond.x * Time.deltaTime), transform.position.y,
-            transform.position.z + (movementPerSecond.z * Time.deltaTime));
+        //move enemy: 
+	    transform.position = new Vector3(transform.position.x + (movementPerSecond.x * Time.deltaTime), transform.position.y,
+        transform.position.z + (movementPerSecond.z * Time.deltaTime));
 
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movementDirection), rotationalForce * Time.deltaTime);
-        }
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movementDirection), rotationalForce * Time.deltaTime);
 	    
     }
 }
